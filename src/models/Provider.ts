4aab146469b2f0b617f4cc441ec0cdc827049b42
Sofi Ferrarotti <sofiferritta@gmail.com>
2024-02-06 16:20:14 -0300

    modifico models/providers.ts.

--- src/models/Provider.ts
+++ src/models/Provider.ts
@@ -1,4 +1,6 @@
-import DbModel from "./DbModel";
+import { InferAttributes, InferCreationAttributes} from'sequelize';
+import { Model, DataType } from 'sequelize-typescript';
+import sequelize from 'services/sequelize';
 
 export type IProvider = {
     id: number|null,
@@ -14,39 +16,38 @@ export type IProvider = {
     daysDelays: Date|number,
     deleted?: boolean,
 }
-export default class Provider extends DbModel implements IProvider{
-    name: string;
-    fantasy_name: string;
-    cuit_cuil: number;
-    email: string;
-    province: string;
-    locality: string;
-    direction: string;
-    phone: number;
-    voucherType: string;
-    daysDelays: Date|number;
-    deleted: boolean;
-    constructor(input:IProvider){
-        super('providers');
-        this.id = input.id;
-        this.name= input.name;
-        this.fantasy_name= input.fantasy_name;
-        this.cuit_cuil= input.cuit_cuil;
-        this.email= input.email;
-        this.province= input.province;
-        this.locality= input.locality;
-        this.direction= input.direction;
-        this.phone= input.phone;
-        this.voucherType= input.voucherType;
-        this.daysDelays= input.daysDelays;
-        this.deleted= input.deleted || false;
-    }
-    static async getAll(offset:string, limit:string){
-        const dbModel = new DbModel('providers');
-        return dbModel.getAll(offset,limit);
-    }
-    static async getById(idProvider:string){
-        const dbModel = new DbModel('providers');
-        return dbModel.getById(idProvider);
-    }
+export class Provider extends Model<InferAttributes<Provider>, InferCreationAttributes<Provider>>{
+    declare id: number|null;
+    declare name: string;
+    declare fantasy_name: string;
+    declare cuit_cuil: number;
+    declare email: string;
+    declare province: string;
+    declare locality: string;
+    declare direction: string;
+    declare phone: number;
+    declare voucherType: string;
+    declare daysDelays: Date|number;
+    declare deleted: boolean;
 }
+Provider.init(
+    {
+        id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true, allowNull: true },
+        name: { type: DataType.STRING, allowNull: false },
+        fantasy_name: { type: DataType.STRING, allowNull: false },
+        cuit_cuil: { type: DataType.INTEGER, allowNull: false },
+        email: { type: DataType.STRING, allowNull: false },
+        province: { type: DataType.STRING, allowNull: false },
+        locality: { type: DataType.STRING, allowNull: false },
+        direction: { type: DataType.STRING, allowNull: false },
+        phone: { type: DataType.INTEGER, allowNull: false },
+        voucherType: { type: DataType.STRING, allowNull: false },
+        daysDelays: { type: DataType.DATE, allowNull: false },
+        deleted: { type: DataType.BOOLEAN, defaultValue: false },
+    },
+    {
+        tableName: 'providers',
+        sequelize,
+    }
+)
+    
