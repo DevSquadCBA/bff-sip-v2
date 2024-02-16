4c78c907045a552a1688167b3702f38b66188686
sirferra <sir.leandroferra@gmail.com>
2024-02-16 10:14:24 -0300

    feat: corrijo la funcion createSale

--- src/functions/sale/createSale.ts
+++ src/functions/sale/createSale.ts
@@ -1,4 +1,5 @@
 import {Sale , ISale } from 'models/Sale';
+import { SaleProduct } from 'models/SaleProduct';
 import { ApiGatewayParsedEvent } from 'types/response-factory/proxies';
 import { Validators } from 'utils/Validator';
 import { LambdaResolver } from 'utils/lambdaResolver';
@@ -7,15 +8,34 @@ interface Event extends ApiGatewayParsedEvent {
 }
 
 const domain = async (event:Event): Promise<{body:number, statusCode:number}> => {
-    const parsedBody = JSON.parse(event.body as unknown as string);
-    const {sale} = parsedBody.sale;
-    const {products} = parsedBody.products;
-    const budget = await Sale.create(sale);
-    //await budget.addProduct(products);
-    return {
-        body: budget.id,
-        statusCode: 200
-    }    
+    try{
+        const parsedBody = JSON.parse(event.body as unknown as string);
+        const {products} = parsedBody;
+        delete parsedBody.products;
+        const sale = parsedBody as ISale;
+        const budget = await Sale.create(sale);
+        if(products.length === 0) return {
+            body: budget.id,
+            statusCode: 200
+        }
+        try{
+            for(const product of products){
+                product.saleId = budget.id;
+                product.productId = product.id;
+                await SaleProduct.create(product);
+            }
+        }catch(e:any){
+            Sale.destroy({where: {id: budget.id}});
+            throw new Error(e);
+        }
+        return {
+            body: budget.id,
+            statusCode: 200
+        }    
+    }catch(e:any){
+        console.error(e);
+        throw new Error(e);
+    }
 }
 
 export const Handler = (event:ApiGatewayParsedEvent)=>LambdaResolver(event, domain, [Validators.OFFSET_AND_LIMITS])
