97464a97d86c35ffe71d9413d481e2c329f05522
sirferra <sir.leandroferra@gmail.com>
2024-02-28 22:34:57 -0300

    feat: devuelvo los productos en getSaleById

--- src/functions/sale/getSaleById.ts
+++ src/functions/sale/getSaleById.ts
@@ -1,5 +1,8 @@
 import { Client } from 'models/Client';
-import { Sale, ISale } from 'models/Sale';
+import { Product } from 'models/Product';
+import { Sale, ISale, SaleWithProduct, ProductsInSale } from 'models/Sale';
+import { SaleProduct } from 'models/SaleProduct';
+import { InvalidIdError } from 'types/errors';
 import { ApiGatewayParsedEvent } from 'types/response-factory/proxies';
 import { Validators } from 'utils/Validator';
 import { LambdaResolver } from 'utils/lambdaResolver';
@@ -10,10 +13,41 @@ interface Event extends ApiGatewayParsedEvent {
 }
 
 const domain = async (event:Event): Promise<{body:ISale, statusCode:number}> => {
-    const budget = await Sale.findByPk(event.pathParameters.idSale, {include: Client})
+    const sale = await Sale.findByPk(event.pathParameters.idSale, {
+        include: [
+            {
+                model: Client,
+            },
+            {
+                model: Product,
+                attributes: ['id', 'code', 'name', 'salePrice', 'purchasePrice'],
+                as: 'products',
+                through: {
+                    model: SaleProduct,
+                    attributes: ['quantity', 'state'],
+                    as: 'saleProducts'
+                } as any
+            }
+        ]
+    })
+    if(!sale){
+        throw new InvalidIdError('La venta no existe');
+    }
+    const saleWithProduct = sale.get({ plain: true });
+    saleWithProduct.products = saleWithProduct.products.map((product:ProductsInSale)=>{
+        return {
+            id: product.id,
+            code: product.code,
+            name: product.name,
+            salePrice: product.salePrice,
+            purchasePrice: product.purchasePrice,
+            quantity: product.saleProducts?.quantity,
+            state: product.saleProducts?.state
+        }
+    });
     
     return {
-        body: budget as ISale,
+        body: saleWithProduct as ISale,
         statusCode: 200
     }
 }
