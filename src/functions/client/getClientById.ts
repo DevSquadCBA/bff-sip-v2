31b7ac9a25337344b0c3f44b518ec0c1b51acded
sirferra <sir.leandroferra@gmail.com>
2024-03-08 01:03:14 -0300

    feat: add totalDue and totalPaid a cliente

--- src/functions/client/getClientById.ts
+++ src/functions/client/getClientById.ts
@@ -8,11 +8,21 @@ interface Event extends ApiGatewayParsedEvent {
         idClient:string
     }
 }
+type ClientResponse = Client & {totalDue:number, totalPaid:number};
 
-const domain = async (event:Event): Promise<{body:Client|null, statusCode:number}> => {
-    const client = await Client.findByPk(event.pathParameters.idClient, {include: Sale})
+const domain = async (event:Event): Promise<{body:ClientResponse|null, statusCode:number}> => {
+    console.log(event.pathParameters.idClient);
+    const client = await Client.findByPk(event.pathParameters.idClient, {include: Sale}) as ClientResponse
+    if(!client){return {body: null, statusCode: 404}}
+    const profitableSales = client?.sales?.map(sale => sale.get({ plain: true }))
+                                        .filter(sale => !['proforma', 'presupuesto'].includes(sale.state));
+    const totalDue = profitableSales?.reduce((acc, sale) => acc + parseFloat(sale.total), 0);
+    const totalPaid = profitableSales?.reduce((acc, sale) => acc + parseFloat(sale.paid), 0);
+    const clientPlain = client.get({ plain: true })
+    clientPlain.totalDue = totalDue;
+    clientPlain.totalPaid = totalPaid;
     return {
-        body: client,
+        body: clientPlain,
         statusCode: 200
     }
 }
