f6df2e0855f0793497cad6afc3a3fdb2c204cb5e
sirferra <sir.leandroferra@gmail.com>
2024-03-08 11:32:52 -0300

    fix: modifico el procedure

--- src/services/generateStoreProcedures.ts
+++ src/services/generateStoreProcedures.ts
@@ -5,7 +5,7 @@ CREATE PROCEDURE spGetAllClientsWithSalesInfo(IN offsetParam INT, IN limitParam 
 BEGIN
     SELECT * FROM (
         SELECT 
-            c.id AS clientId, 
+            c.id, 
             c.name AS \`name\`,
             (SELECT COUNT(*) FROM sales b1 WHERE b1.state != 'finished' AND b1.clientId = c.id AND b1.deleted = 0) AS \`active\`,
             (SELECT COUNT(*) FROM sales b2 WHERE b2.clientId = c.id AND b2.deleted = 0) AS \`total\`,
