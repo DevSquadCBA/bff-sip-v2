431f48f4f6ca784ad5980a009121883628b5507f
sirferra <sir.leandroferra@gmail.com>
2025-03-02 14:18:50 -0300

    feat: agrego account number al bucket para que sea único en aws

--- scripts/manual_persistance.bat
+++ scripts/manual_persistance.bat
@@ -5,16 +5,18 @@ if "%1" == "" (
 ) else (
     set ENVIRONMENT=%1
 )
-set CI_PROJECT_NAME=bff-sip-api-persistance
+set CI_PROJECT_NAME=bff-sip-api
 set STACK=%ENVIRONMENT%-%CI_PROJECT_NAME%
-set BUCKET=%STACK%-deploy-piatti
+for /f "tokens=*" %%a in ('aws sts get-caller-identity --query Account --output text') do set AWS_ACCOUNT_NUMBER=%%a
+set BUCKET=%AWS_ACCOUNT_NUMBER%-%STACK%-deploy-piatti
 set REGION_1=us-east-1
 set PROFILE=default
 set ARN_CERT= CONFIGURA TU ARM AQUÍ (lee el readme)
 
 echo %BUCKET%
 echo "================== Create Bucket =================="
+aws s3api create-bucket --bucket %BUCKET%-persistance
 aws s3api create-bucket --bucket %BUCKET%
 
 echo "================== Build =================="
-sam deploy  --template-file persistance.yaml --no-confirm-changeset --s3-bucket %BUCKET% --region %REGION_1% --profile %PROFILE% --stack-name %STACK% --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ACMCertificateArn=%ARN_CERT%
+@REM sam deploy  --template-file persistance.yaml --no-confirm-changeset --s3-bucket %BUCKET% --region %REGION_1% --profile %PROFILE% --stack-name %STACK% --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ACMCertificateArn=%ARN_CERT%
